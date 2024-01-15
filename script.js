1c4b388875d938162028733e88887cdc6a6f622f
Bintang Syafrian <bintang.syafrian@gmail.com>
2024-01-16 01:36:04 +0700

    Added javascript function for hamburger menu.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const canvas = document.getElementById("particle-canvas");
     const ctx = canvas.getContext("2d");
